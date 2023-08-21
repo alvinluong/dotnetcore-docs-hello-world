dce568b28083621e472b0f838d1174cd962e9a84
alvinluong <142839354+alvinluong@users.noreply.github.com>
2023-08-21 14:58:28 -0700

    5

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,22 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Runtime.InteropServices;
+using System;
+using System.DirectoryServices;
+using System.Web.UI;
 
 namespace dotnetcoresample.Pages;
 
 public class IndexModel : PageModel
 {
 
-    public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
-    
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+   public partial class Index : Page
     {
-        _logger = logger;
-    }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Check if the user is already authenticated
+                if (User.Identity.IsAuthenticated)
+                {
+                    // User is authenticated; you can redirect them to a secure page.
+                    Response.Redirect("SecurePage.aspx");
+                }
+            }
+        }
 
-    public void OnGet()
-    {        
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            string ldapServer = "ldap://10.8.3.15:389"; // LDAP server address
+            string ldapBaseDN = "DC=ladbs,DC=ci,DC=la,DC=ca,DC=us"; // Base DN for your LDAP directory
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+
+            using (DirectoryEntry entry = new DirectoryEntry(ldapServer + "/" + ldapBaseDN, username, password))
+            {
+                try
+                {
+                    // Attempt to bind to the LDAP server with the provided credentials
+                    DirectorySearcher searcher = new DirectorySearcher(entry);
+                    searcher.Filter = "(sAMAccountName=" + username + ")";
+                    SearchResult result = searcher.FindOne();
+
+                    if (result != null)
+                    {
+                        // Authentication successful
+                        FormsAuthentication.RedirectFromLoginPage(username, false);
+                    }
+                    else
+                    {
+                        // Authentication failed
+                        ErrorLabel.Text = "Authentication failed. Please check your username and password.";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ErrorLabel.Text = "An error occurred: " + ex.Message;
+                }
+            }
+        }
     }
 }
